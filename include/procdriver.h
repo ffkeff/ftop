e1346cf18bafe954a3a58c3a8a27225b53ca79fa
Ivan <111454954+ffkeff@users.noreply.github.com>
2024-08-28 18:03:03 +0300

    Update procdriver.h

--- include/procdriver.h
+++ include/procdriver.h
@@ -13,35 +13,6 @@
 #include "../include/ftop_types.h"
 #include "../include/process.h"
 
-#define PROCDIR "/proc"
-
-/************** PROC ROOT ENTRY LIST ****************/
-
-struct
-ProcEntryList {
-    struct ProcEntry    *head;
-    struct ProcEntry    *tail;
-};
-
-// Create new instance ProcEntryList
-static struct ProcEntryList *
-proc_entry_list_init(void);
-
-static struct ProcEntry *
-proc_entry_push(struct ProcEntry *entry, struct ProcEntryList *list);
-
-// Unsafe
-#define proc_entry_list_foreach(entry, entry_list) \
-for (entry = entry_list->head;                     \
-     entry != entry_list->tail;                    \
-     entry = entry->next)
-
-static void
-proc_entry_pop(struct ProcEntryList *list);
-
-static void
-proc_entry_list_free(struct ProcEntryList *list);
-
 
 /************** PROC ROOT OPERATIONS ***************/
 
@@ -50,9 +21,5 @@ get_proc_entries(void);
 
 
 /************** PROC INFO OPERATIONS ***************/
-struct
-    ProcessInfoOperations {
-    char entry_name[MAX_FILE_NAME_LENGHT];
-    void (*read_to) (const char *);
-};
+
 #endif /* procdriver.h */
