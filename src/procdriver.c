f9f9cdf8281515cb9bf64c011472069e3c724e81
Ivan <111454954+ffkeff@users.noreply.github.com>
2024-08-28 18:02:12 +0300

    Update procdriver.c

--- src/procdriver.c
+++ src/procdriver.c
@@ -7,78 +7,15 @@
  *
  */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 
 #include "../include/procdriver.h"
-
-static struct ProcEntryList *
-proc_entry_list_init(void) {
-    struct ProcEntryList    *list;
-
-    list = (struct ProcEntryList*)malloc(sizeof (struct ProcEntryList));
-
-    list->head = NULL;
-    list->tail = NULL;
-
-    return list;
-}
-
-static struct ProcEntry *
-proc_entry_init(void) {
-    static struct ProcEntry *entry;
-
-    entry = (struct ProcEntry *)malloc(sizeof (struct ProcEntry));
-    if (!entry) return NULL;
-
-    memset(entry, 0, MAX_FILE_NAME_LENGHT);
-    entry->type = Undefined;
-    entry->next = NULL;
-    entry->prev = NULL;
-
-    return entry;
-}
-
-static struct ProcEntry *
-proc_entry_push(struct ProcEntry *entry, struct ProcEntryList *list) {
-    if (!entry || !list) return NULL;
-
-    if (NULL == list->head) {
-        list->head = entry;
-        list->tail = entry;
-    } else {
-        entry->prev = list->tail;
-
-        list->tail->next = entry;
-        list->tail = entry;
-    }
-
-    return entry;
-}
-
-static void
-proc_entry_node_pop(struct ProcEntryList *list) {
-    struct ProcEntry     *tmp;
-
-    if (!list) return; //TODO
-
-    tmp = list->tail;
-    list->tail = tmp->prev;
-    (void) free(tmp);
-}
-
-//memory leak!!!
-void
-proc_entry_list_free(struct ProcEntryList *list) {
-    struct ProcEntry    *entry;
-
-    proc_entry_list_foreach(entry, list) {
-        (void) free(entry->prev);
-    }
-
-    (void) free(list);
-}
 
 struct ProcEntryList *
 get_proc_entries(void) {
@@ -121,4 +58,67 @@ get_proc_entries(void) {
     return list;
 }
 
+char *
+read_from(i32 process_id) {
+    char        *path;
+    struct stat st;
+    int         fd;
+    char        *data;
 
+    path = get_full_path(process_id);
+
+    fd = open(path, O_RDONLY, S_IRUSR);
+
+    fstat(fd, &st);
+
+    data = (char*)malloc(st.st_size);
+
+    read(fd, data, st.st_size);
+
+    return data;
+}
+
+struct Stat *
+get_stat_attributes(char *stat) {
+    struct Stat   *attributes;
+
+    attributes = (struct Stat*)malloc(sizeof (struct Stat));
+
+    sscanf(stat,
+           "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld"
+           "%ld %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %d"
+           "%d %u %u %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %d",
+           &attributes->pid, attributes->comm, &attributes->state, &attributes->ppid, &attributes->pgrp,
+           &attributes->session, &attributes->tty_nr, &attributes->tpgid, &attributes->flags, &attributes->minflt,
+           &attributes->cminflt, &attributes->majflt, &attributes->cmajflt, &attributes->utime, &attributes->stime,
+           &attributes->cutime, &attributes->cstime, &attributes->priority, &attributes->nice, &attributes->num_threads,
+           &attributes->itrealvalue, &attributes->starttime, &attributes->vsize, &attributes->rss, &attributes->rsslim,
+           &attributes->startcode, &attributes->endcode, &attributes->startstack, &attributes->kstkesp, &attributes->kstkeip,
+           &attributes->signal, &attributes->blocked, &attributes->sigignore, &attributes->sigcatch, &attributes->wchan,
+           &attributes->nswap, &attributes->cnswap, &attributes->exit_signal, &attributes->processor, &attributes->rt_priority,
+           &attributes->policy, &attributes->delayacct_blkio_ticks, &attributes->guest_time, &attributes->cguest_time, &attributes->start_data,
+           &attributes->end_data, &attributes->start_brk, &attributes->arg_start, &attributes->arg_end, &attributes->env_start,
+           &attributes->env_end, &attributes->exit_code);
+
+    return attributes;
+}
+
+void *
+get_stat_attribute(i32 desc, i32 attribute) {
+
+}
+
+struct Statm *
+get_statm_attributes(FILE *statm) {
+
+}
+
+void *
+get_statm_attribute(i32 desc, i32 attribute) {
+
+}
+
+struct Status *
+get_status_attributes(i32 process_id) {
+
+}
